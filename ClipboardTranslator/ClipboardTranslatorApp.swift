77e10f3c7d9d3c0133b54d24365c8a6b733771e3
Rocky <rocky.hk@gmail.com>
2024-09-03 16:41:25 -0700

    Initial Commit

---
+++ ClipboardTranslator/ClipboardTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardTranslatorApp.swift
+//  ClipboardTranslator
+//
+//  Created by Rocky on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardTranslatorTests/ClipboardTranslatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardTranslatorTests.swift
+//  ClipboardTranslatorTests
+//
+//  Created by Rocky on 9/3/24.
+//
+
+import Testing
+@testable import ClipboardTranslator
+
+struct ClipboardTranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
